ac2b8fce19fcc1d09008fc4abb03de372039b35d
Assela Pathirana <assela@pathirana.net>
2015-05-22 16:17:27 +0200

    changed database connection 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,28 +75,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-on_heroku = False
-if 'DYNO' in os.environ:
-    on_heroku = True
 
-if on_heroku:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.getenv('DATABASE_NAME', 'superlists'),
-            'USER': os.getenv('DATABASE_USER', 'superlists'),
-            'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
-            'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
-            'PORT': os.getenv('DATABASE_PORT', '5432'),
-        }
-    }  
 
 
 # Internationalization
